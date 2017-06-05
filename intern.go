df78b8c23bb67240f75b314115b103a17680725b
Scott Pakin <pakin@lanl.gov>
2017-06-04 23:37:43 -0600

    Make changes to global EqSymbol state thread-safe

--- intern.go
+++ intern.go
@@ -5,6 +5,7 @@ package intern
 
 import (
 	"fmt"
+	"sync"
 )
 
 // An EqSymbol is represented by an integer.  It supports only equality
@@ -14,8 +15,9 @@ type EqSymbol uint64
 
 // eqSymbolState maintains all the state needed to manipulate EqSymbols.
 var eqSymbolState struct {
-	symToStr map[EqSymbol]string // Mapping from EqSymbols to strings
-	strToSym map[string]EqSymbol // Mapping from strings to EqSymbols
+	symToStr     map[EqSymbol]string // Mapping from EqSymbols to strings
+	strToSym     map[string]EqSymbol // Mapping from strings to EqSymbols
+	sync.RWMutex                     // Mutex protecting both of the above
 }
 
 // init initializes our global state.
@@ -27,6 +29,8 @@ func init() {
 // NewEqSymbol maps a string to an EqSymbol.  It guarantees that the same
 // string contents will always return the same EqSymbol.
 func NewEqSymbol(s string) EqSymbol {
+	eqSymbolState.Lock()
+	defer eqSymbolState.Unlock()
 	if sym, ok := eqSymbolState.strToSym[s]; ok {
 		return sym
 	}
@@ -39,9 +43,10 @@ func NewEqSymbol(s string) EqSymbol {
 // String converts an EqSymbol back to a string.  It panics if given an invalid
 // input.
 func (s EqSymbol) String() string {
+	eqSymbolState.RLock()
+	defer eqSymbolState.RUnlock()
 	if str, ok := eqSymbolState.symToStr[s]; ok {
 		return str
-	} else {
-		panic(fmt.Sprintf("%d is not a valid EqSymbol", s))
 	}
+	panic(fmt.Sprintf("%d is not a valid EqSymbol", s))
 }
