b2030fcc26c2908b02158c82e345fec30aaa758a
Scott Pakin <pakin@lanl.gov>
2017-06-10 16:48:11 -0400

    Refactored the performance-testing code in preparation for benchmarking symbols

--- speed_test.go
+++ speed_test.go
@@ -12,19 +12,40 @@ import (
 // Dummy is used to prevent benchmarks from being treated as dead code.
 var Dummy uint64
 
-// Compare a number of long strings that have a substantial prefix in common.
-func BenchmarkCompareSimilarStrings(b *testing.B) {
-	// Create N mostly similar strings.
-	const nComp = 1000 // Number of strings to compare all the others to
-	if b.N < nComp {
-		return // Nothing to do
-	}
-	strs := make([]string, b.N)
+// nComp is the number of strings to compare all the others to.
+const nComp = 1000
+
+// generateSimilarStrings generates a list of strings that have a substantial
+// prefix in common.
+func generateSimilarStrings(n int) []string {
+	strs := make([]string, n)
 	for i := range strs {
 		strs[i] = fmt.Sprintf("String comparisons can be slow when the strings to compare have a long prefix in common.  My favorite number is %15d.", i+1)
 	}
+	return strs
+}
 
-	// Measure the time neede to compare every string to the first string.
+// generateRandomStrings generates a list of long strings with random content.
+func generateRandomStrings(n int) []string {
+	prng := rand.New(rand.NewSource(1920)) // Constant for reproducibility
+	strs := make([]string, n)
+	for i := range strs {
+		nc := prng.Intn(50) + 10 // Number of characters
+		strs[i] = randomString(prng, nc)
+	}
+	return strs
+}
+
+// Compare a number of long strings that have a substantial prefix in common.
+func BenchmarkCompareSimilarStrings(b *testing.B) {
+	// Create N mostly similar strings.
+	if b.N < nComp {
+		return // Nothing to do
+	}
+	strs := generateSimilarStrings(b.N)
+
+	// Measure the time needed to compare each string to each of the first
+	// nComp string.
 	b.ResetTimer()
 	for _, s1 := range strs {
 		for _, s2 := range strs[:nComp] {
@@ -38,18 +59,12 @@ func BenchmarkCompareSimilarStrings(b *testing.B) {
 // Compare a number of long, randomly generated strings.
 func BenchmarkCompareRandomStrings(b *testing.B) {
 	// Create N strings with random contents.
-	prng := rand.New(rand.NewSource(1920)) // Constant for reproducibility
-	const nComp = 1000                     // Number of strings to compare all the others to
 	if b.N < nComp {
 		return // Nothing to do
 	}
-	strs := make([]string, b.N)
-	for i := range strs {
-		nc := prng.Intn(50) + 10 // Number of characters
-		strs[i] = randomString(prng, nc)
-	}
+	strs := generateRandomStrings(b.N)
 
-	// Measure the time neede to compare every string to the first string.
+	// Measure the time needed to compare every string to the first string.
 	b.ResetTimer()
 	for _, s1 := range strs {
 		for _, s2 := range strs[:nComp] {
